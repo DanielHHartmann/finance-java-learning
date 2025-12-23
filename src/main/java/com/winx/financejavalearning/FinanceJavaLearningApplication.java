65e820b50220177c03f2120af0287331a5b8ea78
DanielHHarmann <danielhhartmann@gmail.com>
2025-12-22 22:34:54 -0300

    :rocket: initial commit

---
+++ src/main/java/com/winx/financejavalearning/FinanceJavaLearningApplication.java
@@ -0,0 +1,13 @@
+package com.winx.financejavalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceJavaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceJavaLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winx/financejavalearning/FinanceJavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winx.financejavalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceJavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
